96c1dd98eaf38b537306337d31adbe520bb18dcd
Jamie Paterson <jamiepaterson6715@gmail.com>
2019-08-07 19:30:19 +0100

    Distillery and Whisky controllers added. not working in browser

--- src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -1,16 +1,54 @@
 package com.codeclan.example.WhiskyTracker;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
+
+
 @SpringBootTest
+
+
 public class WhiskyTrackerApplicationTests {
+
+	@Autowired
+	DistilleryRepository distilleryRepository;
+
+	@Autowired
+	WhiskyRepository whiskyRepository;
+
+
 
 	@Test
 	public void contextLoads() {
 	}
+
+
+	@Test
+	public void canFindAllWhiskiesForYear(){
+		List<Whisky> found = whiskyRepository.findWhiskysByYear(2018);
+		assertEquals("The Glendronach Revival", found.get(0).getName());
+		assertEquals("The Glendronach Original", found.get(1).getName());
+
+	}
+
+	@Test
+	public void canGetDistilleriesForHighlandRegion() {
+		List<Distillery> found = distilleryRepository.findDistilleriesByRegion("Highland");
+		assertEquals(1, found.size());
+	}
+
+	
 
 }
